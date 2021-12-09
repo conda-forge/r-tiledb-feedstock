b550c2c66df284b9ad80a4c148f50e4ae1cd1940
Isaiah Norton <isaiah.norton@gmail.com>
2021-12-09 15:48:30 -0500

    Add CXX/CC wrapper to fix up mmacosx-version-min
    The exported deployment target is not being picked up correctly, causing
    the build to fail due to C++17 requirements.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,15 @@
 set -x
 set -o xtrace
+
+if [[ $target_platform  == osx-64 ]]; then
+  export NN_CXX_ORIG=$CXX
+  export NN_CC_ORIG=$CC
+  export CXX=$RECIPE_DIR/cxx_wrap.sh
+  export CC=$RECIPE_DIR/cc_wrap.sh
+  mkdir -p ~/.R
+  echo CC=$RECIPE_DIR/cc_wrap.sh > ~/.R/Makevars
+  echo CXX=$RECIPE_DIR/cxx_wrap.sh >> ~/.R/Makevars
+  echo CXX17=$RECIPE_DIR/cxx_wrap.sh >> ~/.R/Makevars
+fi
+
 $R CMD INSTALL --configure-args=--with-tiledb=${CONDA_PREFIX} --install-tests --build . ${R_ARGS}
