cf59585680b85856ee37f0bc10fd149f317be3f6
Dirk Eddelbuettel <edd@debian.org>
2022-08-19 12:52:53 -0500

    build with standard --no-manual --no-build-vignettes options

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,5 +14,5 @@ fi
 
 ## Build a tarball and install from the tarball is the officially blessed way
 ## It has also respects the .Rbuildignore file allowing us to fine-tune
-$R CMD build .
+$R CMD build --no-manual --no-build-vignettes .
 $R CMD INSTALL --configure-args=--with-tiledb=${CONDA_PREFIX} --install-tests --build tiledb_*.tar.gz ${R_ARGS}
